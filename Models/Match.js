9606c98fd2e29842caca56fbcb955f1aacc014b9
Youssefporsche <youssef.ahmed10computet@gmail.com>
2023-01-07 03:56:17 -0800

    schema

---
+++ Models/Match.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const MatchSchema = new mongoose.Schema({
+  matchNumber: {
+    type: Number,
+    required: true,
+  },
+  roundNumber: {
+    type: Number,
+    required: true,
+  },
+  dateUtc: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  availability:{
+    category1:{
+        count: {type: Number},
+        price: {type: Number},
+        pending: {type: Number}
+    },
+    category2:{
+        count: {type: Number},
+        price: {type: Number},
+        pending: {type: Number}
+    },
+    category3:{
+        count: {type: Number},
+        price: {type: Number},
+        pending: {type: Number}
+    }
+  },
+  homeTeam: {
+    type: String,
+    required: true,
+  },
+  awayTeam: {
+    type: String,
+    required: true,
+  },
+  group: {
+    type: String,
+    required: true,
+  },
+  NumberOfReservedTickets: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Match", MatchSchema);
